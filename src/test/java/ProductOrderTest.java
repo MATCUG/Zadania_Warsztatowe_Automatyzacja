d0fa93f53eeccf2f625afb24239d5dec5e611216
MATCUG <121555640+MATCUG@users.noreply.github.com>
2023-02-24 14:15:55 +0100

    Update ProductOrderTest.java

--- src/test/java/ProductOrderTest.java
+++ src/test/java/ProductOrderTest.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class ProductOrderTest {
+package PACKAGE_NAME;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/resources/features/User-order-test.feature",
+        plugin = {"pretty", "html:report/result.html"})
+public class ProductOrderTest {
 }
